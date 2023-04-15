0ba3ed469f3ef664d572b2753077a153c4b7223c
Abror Nabiddinov <nabiddinovabror8@gmail.com>
2023-04-15 16:47:51 +0500

    code clear

--- HtmlCSS_Exsample/Program.cs
+++ HtmlCSS_Exsample/Program.cs
@@ -4,21 +4,7 @@
     {
         static void Main(string[] args)
         {
-        start:
-            Console.WriteLine("Enter a number between 1 and 10:");
-
-            int number = int.Parse(Console.ReadLine());
-
-            if (number < 1 || number > 10)
-            {
-                Console.Clear();
-                Console.WriteLine("Invalid number. Please try again.");
-                goto start; // transfer control back to the start label
-            }
-            else
-            {
-                Console.WriteLine("You entered: " + number);
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
